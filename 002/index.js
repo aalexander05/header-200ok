7600d3d5642ab1ea465a978a7cc81dcc3315d5af
Greg Tatum <tatum.creative@gmail.com>
2017-12-08 15:32:07 -0600

    002

---
+++ 002/index.js
@@ -0,0 +1,21 @@
+const regl = require('../common/regl')()
+const setupScene = require('./scene')(regl)
+const drawBackground = require('./background')(regl)
+const drawTriangles = require('./triangles')(regl)
+const drawDust = require('./dust')(regl)
+const clear = { depth: 1, color: [0, 0, 0, 1] }
+
+const frameLoop = regl.frame(() => {
+  try {
+    regl.clear(clear)
+    setupScene(({time}) => {
+      // drawBackground()
+      drawDust()
+      drawTriangles()
+    })
+    window.frameDone()
+  } catch (error) {
+    console.error(error)
+    frameLoop.cancel()
+  }
+})
